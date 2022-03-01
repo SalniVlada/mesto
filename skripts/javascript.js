1d5035f2d47801ecb1347af4d113de434b675fe7
Vlada Salinikova <iastoboii@gmail.com>
2022-03-01 21:19:18 +0300

    fix: исправить ошибки после ревью

---
+++ skripts/javascript.js
@@ -0,0 +1,47 @@
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector(".form");
+let nameInput = document.querySelector(".popup__input-name");
+let aboutInput = document.querySelector(".popup__input-about");
+let editButton = document.querySelector(".profile__button");
+let closeButton = document.querySelector(".popup__close");
+let buttonsLike = document.querySelectorAll(".element__button");
+let newPersonName = document.querySelector(".profile__name");
+let about = document.querySelector(".profile__about");
+let inputName = document.querySelector(".popup__input-name");
+let inputAbout = document.querySelector(".popup__input-about");
+
+function showPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function fillPopup() {
+  let nameText = newPersonName.textContent;
+  inputName.setAttribute("value", nameText);
+
+  let aboutText = about.textContent;
+  inputAbout.setAttribute("value", aboutText);
+}
+
+editButton.addEventListener("click", function() {
+  fillPopup();
+  showPopup();
+});
+
+closeButton.addEventListener("click", closePopup);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+
+  let addName = nameInput.value;
+  let addAbout = aboutInput.value;
+  
+  newPersonName.textContent = addName;
+  about.textContent = addAbout;
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
