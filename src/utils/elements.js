33f6f651dc0cfcba652e4aafb7fc436fb31b0569
Vlada Salinikova <iastoboii@gmail.com>
2022-05-05 22:31:22 +0300

    refactor: пересобрать вебпак

---
+++ src/utils/elements.js
@@ -0,0 +1,27 @@
+// объявление данных
+export const elements = [
+  {
+    name: 'Группа Пала',
+    link: 'https://unsplash.com/photos/9TaYFMMapbA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NTQzNw&force=true&w=640'
+  },
+  {
+    name: 'Маттерхорн',
+    link: 'https://unsplash.com/photos/S3zopd8_5OY/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NDUyNQ&force=true&w=640'
+  },
+  {
+    name: 'Вайссхорн',
+    link: 'https://unsplash.com/photos/uqm2GgJANDM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NTMwOA&force=true&w=640'
+  },
+  {
+    name: 'Сьон',
+    link: 'https://unsplash.com/photos/Nr_LsDsyJG4/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NTI2Mw&force=true&w=640'
+  },
+  {
+    name: 'Пассо Ролле',
+    link: 'https://unsplash.com/photos/3i5PHVp1Fkw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NTE5Mg&force=true&w=640'
+  },
+  {
+    name: 'Монблан',
+    link: 'https://unsplash.com/photos/S3zopd8_5OY/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8fGVufDB8fHx8MTY0NzI4NDUyNQ&force=true&w=640'
+  }
+];
