e431f195571dc1b852b4d61e3cd9b56824c8004b
Vlada Salinikova <iastoboii@gmail.com>
2022-05-08 23:07:19 +0300

    fix: исправить ошибки после ревью

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,15 +6,14 @@ export class Popup {
 
   open() {
     this._popup.classList.add("popup_opened");
-    this._setEventListeners();
+    this._currentHandleEscClose = this._handleEscClose.bind(this);
+    document.addEventListener('keydown', this._currentHandleEscClose);
   }
 
   // создание слушателя на нажатие вне попапа для закрытия попапа
-  _setEventListeners() {
+  setEventListeners() {
     this._currentClosePopupWithListener = this._closePopupWithListener.bind(this);
-    this._currentHandleEscClose = this._handleEscClose.bind(this);
     this._popup.addEventListener('click', this._currentClosePopupWithListener);
-    document.addEventListener('keydown', this._currentHandleEscClose);
   }
 
   _closePopupWithListener(evt) {
@@ -32,12 +31,6 @@ export class Popup {
 
   close() {
     this._popup.classList.remove("popup_opened");
-    this._removePopupCloseListener();
-  }
-
-  // удаление слушателя на нажатие вне попапа для закрытия попапа
-  _removePopupCloseListener() {
-    this._popup.removeEventListener('click', this._currentClosePopupWithListener);
     document.removeEventListener('keydown', this._currentHandleEscClose);
   }
 }
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,18 +1,16 @@
 import { Popup } from '../components/Popup.js';
 
 export class PopupWithImage extends Popup {
-  constructor(popupSelector, nameImage, linkImage) {
+  constructor(popupSelector) {
     super(popupSelector);
     this.popupImageTitle = document.querySelector(".popup__image-title");
     this.popupImageLayer = document.querySelector(".popup__image-layer");
-    this.nameImage = nameImage;
-    this.linkImage = linkImage;
   }
 
-  open() {
-    this.popupImageTitle.textContent = this.nameImage;
-    this.popupImageLayer.setAttribute("src", this.linkImage);
-    this.popupImageLayer.setAttribute("alt", this.nameImage);
+  open(nameImage, linkImage) {
+    this.popupImageTitle.textContent = nameImage;
+    this.popupImageLayer.setAttribute("src", linkImage);
+    this.popupImageLayer.setAttribute("alt", nameImage);
 
     super.open();
   }
